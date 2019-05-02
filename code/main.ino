83d4604f02109d5854e65a45e5b0989374fcdaa2
Robin Harris <rh@c4di.net>
2019-05-02 18:14:00 +0100

    Initial commit

---
+++ code/main.ino
@@ -0,0 +1,24 @@
+/* 
+This is the main sketch that drives a portable air quality monitor.
+The components are:
+    SDS011          Particulate sensor
+    GPS module
+    SD Card writer / reader
+    STM32 microprocessor
+
+The device takes readings every 10 seconds and logs
+the time, location and sensor readings to an SD Card
+
+Author:  Robin Harris
+May 2019
+*/
+
+
+
+void setup(){
+
+}
+
+void main(){
+    
+}
