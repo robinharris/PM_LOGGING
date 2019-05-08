44f393d3c73b7aa27378f3ad20dd56ad820329b4
Robin Harris <rh@c4di.net>
2019-05-08 19:48:43 +0100

    Converted to M5Stack - step 1

--- src/main/main.ino
+++ src/main/main.ino
@@ -14,7 +14,8 @@ May 2019
 */
 
 #include <NMEAGPS.h>
-#include <GPSport.h>
+#include <M5Stack.h>
+// #include <GPSport.h>
 
 // globals
 float lat;
@@ -27,7 +28,7 @@ NMEAGPS     gps;
 gps_fix     fix;
 
 void SDS_Setup(){
-    gpsPort.begin(9600); // this will default to Serial1 if it exists
+    Serial2.begin(9600); // this will default to Serial1 if it exists
     // this uses AltSoftwareSerial and pins 8 & 9 on an Uno
 }
 
@@ -38,39 +39,46 @@ void loopGPS(){
     int day;
     int mnth;
     int year;
-    while (gps.available( gpsPort )) {
+    while (gps.available( Serial2 )) {
         fix = gps.read();
-
-        Serial.print( F("Location: ") );
         if (fix.valid.location) {
+            Serial.print( F("Location: ") );
             Serial.print( fix.latitude(), 6 );
             Serial.print( ',' );
             Serial.print( fix.longitude(), 6 );
-    }
+            M5.Lcd.setCursor(5,50);
+            M5.Lcd.printf("Location: ");
+            M5.Lcd.print( fix.latitude(), 6 );
+            M5.Lcd.printf( "," );
+            M5.Lcd.println( fix.longitude(), 6 );
+            M5.update();
+        }
+        if (fix.valid.altitude)
+            Serial.print( F(", Altitude: ") );
+            Serial.print( fix.altitude() );
+            Serial.println();
+            M5.Lcd.printf(", Altitude: ");
+            M5.Lcd.print( fix.altitude() );
+            M5.update();
 
-    Serial.print( F(", Altitude: ") );
-    if (fix.valid.altitude)
-        Serial.print( fix.altitude() );
+        // set global variables
+        lat = fix.latitude();
+        lon = fix.longitude();
+        alt = fix.altitude();
 
-    Serial.println();
+        // set local variables for date and time
+        year = fix.dateTime.year;
+        mnth = fix.dateTime.month;
+        day = fix.dateTime.day;
+        hr = fix.dateTime.hours;
+        min = fix.dateTime.minutes;
+        sec = fix.dateTime.seconds;
 
-    // set global variables
-    lat = fix.latitude();
-    lon = fix.longitude();
-    alt = fix.altitude();
-
-    // set local variables for date and time
-    year = fix.dateTime.year;
-    mnth = fix.dateTime.month;
-    day = fix.dateTime.day;
-    hr = fix.dateTime.hours;
-    min = fix.dateTime.minutes;
-    sec = fix.dateTime.seconds;
-
-    // create a global timestamp as a string
-    sprintf(timestampString, "%d:%02d:%02d %02d:%02d:%02d", year, mnth, day, hr, min, sec);
-    Serial.println(timestampString);
-    return;
+        // create a global timestamp as a string
+        sprintf(timestampString, "%d:%02d:%02d %02d:%02d:%02d", year, mnth, day, hr, min, sec);
+        Serial.println(timestampString);
+        M5.Lcd.setCursor(5,100);
+        M5.Lcd.print(timestampString);
     }
 }
 
@@ -85,6 +93,12 @@ void loopSDS(){
 void setup(){
     SDS_Setup();
     Serial.begin(115200);
+    Serial2.begin(9600, SERIAL_8N1, 16, 17);
+    M5.begin();
+    M5.Lcd.setCursor(5,5);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.printf("Starting to listen");
+    M5.update();
     Serial.println(F("Starting main sketch"));
 }
 
